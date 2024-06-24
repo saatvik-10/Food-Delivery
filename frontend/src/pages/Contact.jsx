b3c63cb7a2f0d65cbe95e99c458ead58110984fa
saatvik-10 <saatvikmadan529@gmail.com>
2024-06-24 20:26:27 +0200

    fixes in contact section

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -6,28 +6,28 @@ const About = () => {
   return (
     <div className="container flex flex-col items-center justify-center pt-10 md:space-y-0 md:pt-0">
       <div className="">
-        <img src={image} alt="" className="md:h-60" />
+        <img src={image} alt="" className="md:h-72" />
       </div>
       <div className="text-xl">
-        <p className="mb-2 text-lg md:text-2xl">
+        <p className="mb-6 text-lg md:text-3xl">
           We value your feedback and inquiries. Please feel free to reach out to
           us with any questions, concerns, or feedback you may have. Our
           dedicated customer support team is here to assist you.
         </p>
-        <h1 className="mb-1 text-xl font-medium md:text-3xl">
+        <h1 className="mb-1 text-xl font-medium md:text-4xl">
           Customer Support Hours:
         </h1>
-        <span className="text-base md:text-xl">
+        <span className="text-base md:text-2xl">
           Monday - Saturday: 9:00 AM - 10:00 PM
         </span>
         <br />
-        <span className="text-base md:text-xl">Closed on Sunday</span>
-        <h1 className="my-2 text-xl font-medium md:text-3xl">
+        <span className="text-base md:text-2xl">Closed on Sunday</span>
+        <h1 className="mt-4 text-xl font-medium md:text-4xl">
           Contact Information:
         </h1>
-        <span className="text-base md:text-xl">Phone: 1234567890</span>
+        <span className="text-base md:text-2xl">Phone: 1234567890</span>
         <br />
-        <span className="text-base md:text-xl">Email:</span>
+        <span className="text-base md:text-2xl">Email:</span>
         <Link
           to="https://mail.google.com/mail/?view=cm&fs=1&to=foodyzzz@gmail.com"
           target="_blank"
@@ -35,7 +35,7 @@ const About = () => {
         >
           foodyzzz@gmail.com
         </Link>
-        <p className="mt-4 text-center text-xl md:text-4xl">
+        <p className="mt-6 text-center text-xl md:text-4xl">
           Thank you for choosing <b className="text-orange-600">FoodyZzz</b>. We
           look forward to hearing from you!
         </p>
