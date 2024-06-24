2f16c0796010169f3854393d7fd64194b021da40
saatvik-10 <saatvikmadan529@gmail.com>
2024-06-24 14:25:41 +0200

    fixes in contact section

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,36 +1,42 @@
-import React from 'react';
-import image from '../components/images/contact/contact.jpg';
-import { Link } from 'react-router-dom';
+import React from "react";
+import image from "../components/images/contact/contact.png";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div className='container flex flex-col items-center px-6 mx-auto space-y-0 md:space-y-0'>
-      <div className='grid place-content-center'>
-        <img src={image} alt='' className='h-48' />
+    <div className="container flex h-[100svh] flex-col items-center justify-center pt-10 md:space-y-0 md:pt-0">
+      <div className="">
+        <img src={image} alt="" className="md:h-60" />
       </div>
-      <div className='text-xl'>
-        <p>
+      <div className="text-xl">
+        <p className="mb-3 text-lg md:text-2xl">
           We value your feedback and inquiries. Please feel free to reach out to
           us with any questions, concerns, or feedback you may have. Our
           dedicated customer support team is here to assist you.
         </p>
+        <h1 className="mb-1 text-xl font-medium md:text-3xl">
+          Customer Support Hours:
+        </h1>
+        <span className="text-base md:text-xl">
+          Monday - Saturday: 9:00 AM - 10:00 PM
+        </span>
         <br />
-        <h1 className='text-3xl font-medium mb-3'>Customer Support Hours:</h1>
-        Monday - Friday: 9:00 AM - 6:00 PM (IST)
+        <span className="text-base md:text-xl">Closed on Sunday</span>
+        <h1 className="my-2 text-xl font-medium md:text-3xl">
+          Contact Information:
+        </h1>
+        <span className="text-base md:text-xl">Phone: 1234567890</span>
         <br />
-        Saturday - Sunday: Closed
-        <h1 className='text-3xl font-medium my-3'>Contact Information:</h1>
-        Phone: 1234567890
-        <br />
-        Email:
+        <span className="text-base md:text-xl">Email:</span>
         <Link
-          to='https://mail.google.com/mail/?view=cm&fs=1&to=foodyzzz@gmail.com'
-          target='_blank'
+          to="https://mail.google.com/mail/?view=cm&fs=1&to=foodyzzz@gmail.com"
+          target="_blank"
+          className="text-base underline md:text-xl"
         >
           foodyzzz@gmail.com
         </Link>
-        <p className='text-4xl mt-4'>
-          Thank you for choosing <b className='text-orange-600'>FoodyZzz</b> We
+        <p className="mt-4 text-center text-xl md:text-4xl">
+          Thank you for choosing <b className="text-orange-600">FoodyZzz</b>. We
           look forward to hearing from you!
         </p>
       </div>
