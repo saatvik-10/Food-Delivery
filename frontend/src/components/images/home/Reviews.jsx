27963ed2780db665883449ef76b48812441d845c
saatvik-10 <saatvikmadan529@gmail.com>
2024-06-24 22:22:21 +0200

    addition in the home page

---
+++ frontend/src/components/images/home/Reviews.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Card } from "../../ui/card";
+
+const reviews = [
+  {
+    name: "Anandita Babar",
+    date: "Feb 9, 2024",
+    review:
+      "As a customer, I had an amazing experience on this website. The user-friendly interface made it easy to explore the delicious food options, and the detailed descriptions helped me choose the perfect dishes. The checkout process was smooth, and my order arrived promptly, with every item tasting fresh and flavorful. Overall, I'm very impressed with the quality of both the website and the food, and I'll definitely be returning for more.",
+  },
+  {
+    name: "Chriag Bhalotia",
+    date: "June 30, 2024",
+    review:
+      "As a first-time customer, I was thoroughly impressed with the entire experience. The website was easy to use, and the food exceeded my expectations. The dishes were flavorful and arrived hot and fresh. I will definitely be ordering again!",
+  },
+  {
+    name: "Ishan Magarde",
+    date: "Nov 26, 2024",
+    review:
+      "I had a fantastic experience ordering from this website. The interface is user-friendly, making it easy to browse through the wide variety of dishes available. My order arrived promptly, and the food was hot, fresh, and bursting with flavor. The attention to detail and quality of service were impressive. I will definitely be recommending this to my friends and family!",
+  },
+];
+
+export default function Reviews() {
+  return (
+    <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
+      {reviews.map((item, key) => (
+        <Card key={key} className="rounded-xl border-2 border-red-600 p-4">
+          <div className="mb-4 flex items-center justify-between">
+            <span className="text-md font-semibold text-orange-600 md:text-xl">
+              {item.name}
+            </span>
+            <span className="text-xs text-gray-600 md:text-sm">
+              {item.date}
+            </span>
+          </div>
+          <p className="text-sm font-medium md:text-base">{item.review}</p>
+        </Card>
+      ))}
+    </div>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 import img1 from "../components/images/home/hfood.png";
 import { Link } from "react-router-dom";
+import Reviews from "../components/images/home/Reviews";
 
 export default function Home() {
   return (
-    <div className="container flex flex-col-reverse px-6 md:h-auto md:flex-row md:space-y-0 lg:flex-row">
-      <div className="flex flex-col space-y-6 md:w-1/2 md:space-y-12">
-        <h1 className="my-4 max-w-lg text-center text-5xl font-bold text-orange-600 md:my-0 md:text-left md:text-8xl">
-          FoodyZzz...
-        </h1>
-        <p className="max-w-xl text-start text-lg font-semibold text-blue-500 md:text-left md:text-2xl">
-          "Discover the ease of gourmet dining from home! Browse our curated
-          menu and indulge in a feast of flavors delivered straight to your
-          doorstep. From mouthwatering entrees to decadent desserts, elevate
-          your dining experience with our convenient food delivery service.
-          Order now and let us bring the restaurant experience to you!"
-        </p>
-        <p className="text-center text-xl font-semibold text-black hover:scale-110 md:text-start md:text-2xl lg:text-3xl">
-          <Link to="/menu">Click here to order yours now !</Link>
-        </p>
+    <div className="container">
+      <div className="flex flex-col-reverse md:flex-row md:space-y-0 md:py-20">
+        <div className="flex flex-col space-y-6 md:w-1/2 md:space-y-12">
+          <h1 className="my-4 max-w-lg text-center text-5xl font-bold text-orange-600 md:my-0 md:text-left md:text-8xl">
+            FoodyZzz...
+          </h1>
+          <p className="max-w-xl text-start text-lg font-semibold text-blue-500 md:text-left md:text-2xl">
+            "Discover the ease of gourmet dining from home! Browse our curated
+            menu and indulge in a feast of flavors delivered straight to your
+            doorstep. From mouthwatering entrees to decadent desserts, elevate
+            your dining experience with our convenient food delivery service.
+            Order now and let us bring the restaurant experience to you!"
+          </p>
+          <p className="text-center text-xl font-semibold text-black hover:scale-110 md:text-start md:text-2xl lg:text-3xl">
+            <Link to="/menu">Click here to order yours now !</Link>
+          </p>
+        </div>
+        <div className="md:w-1/2">
+          <img src={img1} alt="" />
+        </div>
       </div>
-      <div className="md:w-1/2">
-        <img src={img1} alt="" />
+      <div className="">
+        <span className="mb-14 mt-5 flex items-center justify-center text-center text-2xl font-semibold text-red-600 md:text-6xl">
+          From Our Regulars
+        </span>
+        <Reviews />
       </div>
     </div>
   );
