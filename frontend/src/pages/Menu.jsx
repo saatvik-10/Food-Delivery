a6f5b82aa67f774c779456119a1c1a173920854c
saatvik-10 <saatvikmadan529@gmail.com>
2024-06-25 09:35:09 +0200

    added non veg starters sectin

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Separator } from "../components/ui/separator";
 import VegStarters from "../props/vegStarters/page";
 import VegMainCourse from "../props/vegMainCourse/page";
+import NonVegStarter from "../props/nonVegStarters/page";
 
 function Menu() {
   return (
-    <div className="container">
+    <div className="container my-5">
       <VegStarters title={"VEG STARTERS"} />
       <Separator className="my-16" />
       <VegMainCourse title={"VEG MAIN COURSE"} />
+      <Separator className="my-16" />
+      <NonVegStarter title={"NON-VEG STARTERS"} />
       <Separator className="my-16" />
     </div>
   );
--- frontend/src/props/nonVegStarters/page.jsx
+++ frontend/src/props/nonVegStarters/page.jsx
@@ -1,13 +1,60 @@
-import React from 'react';
+import React from "react";
+import MenuCards from "../../components/Menu/MenuCards";
+import GKebab from "../../components/images/menu/NonVegItems/Starters/GKebab.jpg";
+import TChicken from "../../components/images/menu/NonVegItems/Starters/TChicken.jpg";
+import CKebab from "../../components/images/menu/NonVegItems/Starters/CKebab.jpg";
+import FTikka from "../../components/images/menu/NonVegItems/Starters/FTikka.jpg";
+import CAfghani from "../../components/images/menu/NonVegItems/Starters/CAfghani.jpg";
+import CLollipop from "../../components/images/menu/NonVegItems/Starters/CLollipop.jpg";
+
+const carouselItems = [
+  {
+    image: GKebab,
+    subTitle: "Mutton Galouti",
+    description:
+      "From Lucknow, Delicately spiced and melt-in-the-mouth mutton kebabs, made with finely minced meat and aromatic spices, known for their velvety texture.",
+  },
+  {
+    image: TChicken,
+    subTitle: "Tandoori Chicken",
+    description:
+      "Marinated chicken pieces cooked in a tandoor (clay oven), characterized by their smoky flavor and vibrant red hue from spices and yogurt.",
+  },
+  {
+    image: CKebab,
+    subTitle: "Chicken Kebab",
+    description:
+      "Juicy and flavorful skewers of marinated chicken, grilled or baked, often enjoyed with mint chutney and lemon wedges.",
+  },
+  {
+    image: FTikka,
+    subTitle: "Fish Tikka",
+    description:
+      "Succulent fish chunks marinated in a blend of yogurt and spices, then grilled or baked to perfection, offering a smoky and spicy taste.",
+  },
+  {
+    image: CAfghani,
+    subTitle: "Chicken Afghani",
+    description:
+      "Creamy and mildly spiced chicken dish marinated with cream, yogurt, and subtle spices, known for its rich and delicate flavors.",
+  },
+  {
+    image: CLollipop,
+    subTitle: "Chicken Lollipop",
+    description:
+      "A popular appetizer made from chicken wings frenched to resemble lollipops, marinated and deep-fried to a crispy finish.",
+  },
+];
 
 function NonVegStarter({ title }) {
   return (
-    <div>
-      <h1 className='text-5xl font-bold text-orange-600 mb-4 text-center'>
+    <div className="">
+      <h2 className="my-3 flex items-center justify-center font-serif text-2xl font-semibold text-orange-600 md:text-6xl">
         {title}
-      </h1>
+      </h2>
+      <MenuCards carouselItems={carouselItems} />
     </div>
   );
 }
 
-export default VegStarters;
+export default NonVegStarter;
