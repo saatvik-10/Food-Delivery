1232490124326a48237ca4357723ff29f46cd068
saatvik-10 <saatvikmadan529@gmail.com>
2024-06-30 19:27:15 +0200

    addition of address field

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -23,6 +23,7 @@ import axios from "axios";
 import { DatePicker } from "../components/DatePicker/DatePicker";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
+import { Textarea } from "../components/ui/textarea";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -45,6 +46,9 @@ const formSchema = z.object({
   forgotPasswordDate: z.date({
     message: "Date is required for Forgot Password.",
   }),
+  address: z.string().min(1, {
+    message: "Address is required",
+  }),
 });
 
 const RegisterForm = () => {
@@ -58,6 +62,7 @@ const RegisterForm = () => {
       password: "",
       confirmPassword: "",
       forgotPasswordDate: null,
+      address: "",
     },
   });
 
@@ -82,6 +87,7 @@ const RegisterForm = () => {
           email: data.email,
           password: data.password,
           date: data.forgotPasswordDate,
+          address: data.address,
         },
       );
       console.log(response.data);
@@ -118,7 +124,7 @@ const RegisterForm = () => {
   }, [navigate]);
 
   return (
-    <div className="mx-auto md:w-1/3">
+    <div className="mx-auto md:w-3/4">
       <Card className="border-2 border-red-600">
         <CardHeader>
           <CardTitle className="text-center">Register</CardTitle>
@@ -130,108 +136,131 @@ const RegisterForm = () => {
           <Form {...form}>
             <form
               onSubmit={form.handleSubmit(handleSubmit)}
-              className="space-y-2"
+              className="grid grid-cols-1 space-y-2 md:grid-cols-2 md:gap-6"
             >
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-white">
-                      Name
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        className="focus-visible: border-0 bg-slate-100 text-black ring-offset-0 focus-visible:ring-0 dark:bg-slate-500 dark:text-white"
-                        placeholder="Enter Name"
+              <div>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-white">
+                        Name
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="focus-visible: border-0 bg-slate-100 text-black ring-offset-0 focus-visible:ring-0 dark:bg-slate-500 dark:text-white"
+                          placeholder="Enter Name"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-white">
+                        Email
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="focus-visible: border-0 bg-slate-100 text-black ring-offset-0 focus-visible:ring-0 dark:bg-slate-500 dark:text-white"
+                          placeholder="Enter Email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-white">
+                        Password
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          className="focus-visible: border-0 bg-slate-100 text-black ring-offset-0 focus-visible:ring-0 dark:bg-slate-500 dark:text-white"
+                          placeholder="Enter Password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-white">
+                        Confirm Password
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          className="focus-visible: border-0 bg-slate-100 text-black ring-offset-0 focus-visible:ring-0 dark:bg-slate-500 dark:text-white"
+                          placeholder="Enter Confirm Password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="relative">
+                <FormField
+                  control={form.control}
+                  name="forgotPasswordDate"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col">
+                      <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-white">
+                        What is that one Date ?
+                      </FormLabel>
+                      <div className="text-xs text-zinc-500 dark:text-white">
+                        (For Forgot Password)
+                      </div>
+                      <DatePicker {...field} />
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="address"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col">
+                      <FormLabel className="mt-3 text-xs font-bold uppercase text-zinc-500 dark:text-white">
+                        Address
+                      </FormLabel>
+                      <Textarea
+                        className="focus-visible: h-20 border-0 bg-slate-100 text-black ring-offset-0 focus-visible:ring-0 dark:bg-slate-500 dark:text-white"
+                        placeholder="Enter Address"
                         {...field}
                       />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-white">
-                      Email
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        className="focus-visible: border-0 bg-slate-100 text-black ring-offset-0 focus-visible:ring-0 dark:bg-slate-500 dark:text-white"
-                        placeholder="Enter Email"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-white">
-                      Password
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        type="password"
-                        className="focus-visible: border-0 bg-slate-100 text-black ring-offset-0 focus-visible:ring-0 dark:bg-slate-500 dark:text-white"
-                        placeholder="Enter Password"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="confirmPassword"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-white">
-                      Confirm Password
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        type="password"
-                        className="focus-visible: border-0 bg-slate-100 text-black ring-offset-0 focus-visible:ring-0 dark:bg-slate-500 dark:text-white"
-                        placeholder="Enter Confirm Password"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="forgotPasswordDate"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-white">
-                      What is that one Date ?
-                    </FormLabel>
-                    <div className="text-xs text-zinc-500 dark:text-white">
-                      (For Forgot Password)
-                    </div>
-                    <DatePicker {...field} />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <Button className="w-full">Sign In</Button>
+                <Button className="bottom-0 mt-3 w-full md:absolute md:mt-0">
+                  Sign Up
+                </Button>
+              </div>
             </form>
           </Form>
         </CardContent>
